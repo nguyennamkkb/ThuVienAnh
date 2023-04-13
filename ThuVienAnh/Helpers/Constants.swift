47941b8304a66dd57f61f95a7dc551fe8b9cb943
namnl <>
2023-04-13 18:00:55 +0700

    plash screen

---
+++ ThuVienAnh/Helpers/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  ThuVienAnh
+//
+//  Created by cvcuong on 13/04/2023.
+//
+
+import Foundation
+
+let imagePath: String = "/Resources/Images/"
+let iconPath: String = "Resources/Icons/"
