83337f5e5170080d40b55f5b104d1ab6361a53b2
namnl <>
2023-04-20 18:08:30 +0700

    categoryCell

---
+++ ThuVienAnh/Models/Category.swift
@@ -0,0 +1,25 @@
+//
+//  Category.swift
+//  ThuVienAnh
+//
+//  Created by cvcuong on 20/04/2023.
+//
+
+import Foundation
+import ObjectMapper
+
+class Category: Mappable {
+    var id: Int64?
+    var name: String?
+    var image: String?
+    var status: Int?
+    required init?(map: Map) {
+        mapping(map: map)
+    }
+    func mapping(map: Map) {
+        id <- map["id"]
+        name <- map["name"]
+        image <- map["image"]
+        status <- map["status"]
+    }
+}
---
+++ ThuVienAnh/ViewControllers/CategoryVC/CategoryCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCell.swift
+//  ThuVienAnh
+//
+//  Created by cvcuong on 20/04/2023.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
